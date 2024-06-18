09b2fddfb0535f670d2d9136dc20d4a74e08d8f4
Ian <ian@hibirdi.com>
2024-06-18 16:13:38 +0100

    add utils file

---
+++ utils.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+
+def wait_for_files_active(files):
+    """Waits for the given files to be active.
+
+    Some files uploaded to the Gemini API need to be processed before they can be
+    used as prompt inputs. The status can be seen by querying the file's "state"
+    field.
+
+    This implementation uses a simple blocking polling loop. Production code
+    should probably employ a more sophisticated approach.
+    """
+    print("Waiting for file processing...")
+    for name in (file.name for file in files):
+        file = genai.get_file(name)
+        while file.state.name == "PROCESSING":
+            print(".", end="", flush=True)
+            time.sleep(10)
+            file = genai.get_file(name)
+        if file.state.name != "ACTIVE":
+            raise Exception(f"File {file.name} failed to process")
+    print("...all files ready")
+    print()
