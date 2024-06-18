ebb8a646db2dfab59436bc67b48ea0397e71b3ab
Ian <ian@hibirdi.com>
2024-06-18 19:37:31 +0100

    return code to original

--- utils.py
+++ utils.py
@@ -19,12 +19,12 @@ def wait_for_files_active(files):
     should probably employ a more sophisticated approach.
     """
     print("Waiting for file processing...")
-    for file_ in files:
-        file = genai.get_file(file_.name)
+    for name in (file.name for file in files):
+        file = genai.get_file(name)
         while file.state.name == "PROCESSING":
             print(".", end="", flush=True)
             time.sleep(10)
-            file = genai.get_file(file_.name)
+            file = genai.get_file(name)
         if file.state.name != "ACTIVE":
             raise Exception(f"File {file.name} failed to process")
     print("...all files ready")
